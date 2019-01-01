a58fbf34c3878eb5f39229fdbc7e2b6b0c1601f3
truar <thibault.ruaro@gmail.com>
2019-01-01 18:13:49 +0100

    Initial Version

---
+++ src/test/java/com/cloudcmr/app/CloudCmrAppTests.java
@@ -0,0 +1,17 @@
+package com.cloudcmr.app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CloudCmrAppTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
